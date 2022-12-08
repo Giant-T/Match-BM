d18ccd8b85858fc255f65ce7be8684c7b94a270b
William Boudreault <boudreaultwilliam@gmail.com>
2022-12-08 00:05:56 -0500

    Affichage home user

--- lib/pages/user_home.dart
+++ lib/pages/user_home.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:match_bm/components/loader.dart';
+import 'package:match_bm/components/page_title.dart';
 import 'package:match_bm/database/firestore.dart';
 import 'package:match_bm/models/user_model.dart';
+import 'package:sprintf/sprintf.dart';
 
 import '../models/page_container.dart';
 
@@ -10,7 +12,7 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class UserHome extends StatelessWidget {
   UserHome({Key? key}) : super(key: key);
-  late UserModel? userInfo;
+  late UserModel? userInfo = UserModel("", "", "");
 
   Future<UserModel?> getUserInfo() async {
     userInfo = await FireStore.getUser(_auth.currentUser!.uid);
@@ -27,14 +29,18 @@ class UserHome extends StatelessWidget {
           }
 
           return PageContainer(
-            body: Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(userInfo!.firstname),
-                  ]),
-            ),
-          );
+              body: Padding(
+            padding: const EdgeInsets.only(top: 60, left: 5, right: 5),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  PageTitle(
+                    text: sprintf("Bonjour %s %s!",
+                        [userInfo!.firstname, userInfo!.lastname]),
+                  )
+                ]),
+          ));
         }));
   }
 }
